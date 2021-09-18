1b22441d3d6112cd1aa472ba71c08647962eff1e
Carlos Andres Charris S <darkklitos@gmail.com>
2021-09-17 20:47:37 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    host: req.headers.host,
+    descripcion: 'Metodo GET',
+    query: req.query,
+    method: req.method,
+    status: res.statusCode
+  })
+})
+
+app.post('/', (req, res) => {
+  res.status(200).send({
+    host: req.headers.host,
+    descripcion: 'Metodo POST',
+    request: req.body,
+    method: req.method,
+    status: res.statusCode
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server in port: http://127.0.0.1:${port}`);
+})
